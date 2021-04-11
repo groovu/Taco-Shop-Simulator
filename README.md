17d01602a12eab94dc95ac5dfa137273e20cab33
Cherish Truong <groovu@gmail.com>
2021-04-11 02:30:56 -0700

    Init TSS repo

---
+++ README.md
@@ -0,0 +1,10 @@
+# Taco Shop Simulator
+A taco shop management game.
+
+# Todo
+Implement admin system to view and modify user information
+Figure out how to use an online database.  (How to migrate local db?)
+Implement buy menu
+Implement research menu
+Implement hiring / training menu
+Leaderboard?
---
+++ scratch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scratch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //add reference to db (ADO > select db)
+            //init db
+            //retrieve row using lambda fxn
+            //modify like a normal object
+            //profit
+            TacoDBEntities taco = new TacoDBEntities();
+            var table = taco.testTables;
+
+            var result = table.SingleOrDefault(b => b.id == 1);
+
+            result.blah = "1000";
+            taco.SaveChanges();
+
+        }
+    }
+}
