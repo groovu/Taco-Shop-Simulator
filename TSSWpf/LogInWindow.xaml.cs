0eb8c0dd7ccb21b99336e231eac74526d3c72584
Cherish Truong <groovu@gmail.com>
2021-04-11 18:26:28 -0700

    login works, admin panel created.  todo: connect db to login to check username/hashed(password).

--- TSSWpf/LogInWindow.xaml.cs
+++ TSSWpf/LogInWindow.xaml.cs
@@ -7,11 +7,44 @@ namespace TSSWpf
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
+        bool debug = true;
         public MainWindow()
         {
+            debugCheck(debug);
             InitializeComponent();
+        }
+
+        private void login_button_Click(object sender, RoutedEventArgs e)
+        {
+            string user = loginUsernameBox.Text;
+            string password = loginPasswordBox.Password;
+
+            if (user == "user" && password == "password")
+            {
+                System.Windows.MessageBox.Show("Correct");
+                this.Hide();
+                Window w = new AdminWindow();
+                w.Show();
+
+            } else
+            {
+                System.Windows.MessageBox.Show("Incorrect username or password");
+                //add to log?
+                if (debug)
+                {
+                    System.Windows.MessageBox.Show(user + "; " + password);
+                }
+            }
+        }
+        private void debugCheck(bool debug)
+        {
+            if (debug)
+            {
+                System.Windows.MessageBox.Show("Program is in debug mode");
+            } 
         }
     }
 }
